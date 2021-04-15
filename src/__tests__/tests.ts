45f9abfc4d2254c0ad2f9ac185e1ce9db2284b07
Chris Fogelklou <chris.fogelklou@gmail.com>
2021-04-15 07:33:37 +0200

    Add unit tests

---
+++ src/__tests__/tests.ts
@@ -0,0 +1,34 @@
+import { deepEqual } from '../utils';
+
+export const dummyface = 88;
+
+function LOG(message?: any, ...optionalParams: any[]) {
+  //console.log(message, optionalParams);
+}
+
+it('deepEqual', () => {
+  {
+    let a = [1, 2, 3];
+    let b = [...a];
+    expect(deepEqual(a, b)).toEqual(true);
+    b[0]++;
+    expect(deepEqual(a, b)).toEqual(false);
+    a[0]++;
+    expect(deepEqual(a, b)).toEqual(true);
+    let c = { ...a };
+    expect(deepEqual(a, c)).toEqual(false);
+    let d = c;
+    expect(deepEqual(c, d)).toEqual(true);
+    d = { ...c };
+    expect(deepEqual(c, d)).toEqual(true);
+  }
+  {
+    let a = [1, 2, { three: [1, 2, 3] }];
+    let b = [1, 2, { three: [1, 2, 3] }];
+    expect(deepEqual(a, b)).toEqual(true);
+    let c = [1, 2, { three: [1, 2, 'three'] }];
+    expect(deepEqual(a, c)).toEqual(false);
+    let d = [1, 2, { three: [1, 2, -3] }];
+    expect(deepEqual(a, d)).toEqual(false);
+  }
+});
